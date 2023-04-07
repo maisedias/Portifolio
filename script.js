17669ac13fb19d1d951fb8f728347e14b6df9265
Maise Dias <maise.sistemas@gmail.com>
2023-04-07 11:53:08 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.multiple-text', {
+    strings: ['Desenvolvedora Frontend', 'Designer', 'Blogueira'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    fadeOut: true,
+    loop: true
+  });
+    
